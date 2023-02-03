98127afd917ee2aada17e95ef9f29fa2b19645e7
Ivan Golubev <zxcgi@pm.me>
2023-02-03 22:43:08 +0000

    Testing cgtlf
    Testing cgtlf

--- projects/Common/src/ModelLoader.cpp
+++ projects/Common/src/ModelLoader.cpp
@@ -8,6 +8,7 @@ module;
 #include <format>
 #include <string>
 #include <vector>
+#include <cgltf/cgltf.h>
 module ModelLoader;
 
 import Logging;
@@ -19,6 +20,7 @@ using DirectX::XMFLOAT2;
 using gg::Mesh;
 using gg::Model;
 using gg::AssetLoadException;
+using gg::BreakIfFalse;
 
 namespace
 {
@@ -70,6 +72,67 @@ namespace
 			outModel.meshes.emplace_back(readMesh(scene->mMeshes[i], scene));
 		return true;
 	}
+
+	bool LoadMeshesCGLTF(std::string const& modelAbsolutePath, Model& outModel)
+	{
+		cgltf_options options{ 0 };
+		cgltf_data* data{ nullptr };
+
+		cgltf_result result = cgltf_parse_file(&options, modelAbsolutePath.c_str(), &data);
+		if (result != cgltf_result_success)
+			return false;
+
+		//result = cgltf_load_buffers(&options, data, data->meshes[0].name);
+		//if (result != cgltf_result_success)
+		//	return false;
+
+		result = cgltf_validate(data);
+		if (result != cgltf_result_success)
+			return false;
+
+		// TODO: wow, we can read the attributes from the .glb files too, sick !
+		// TODO: read the name of the mesh too, why not
+		// TODO: we can actually read transforms of each game object from .glb and export whole scenes from Blender, awesome !
+
+		/* expect at least one mesh in the model */
+		BreakIfFalse(data->meshes_count > 0);
+		cgltf_mesh* mesh = &data->meshes[0];
+
+		/* expect at least one primitive in the mesh */
+		BreakIfFalse(mesh->primitives_count > 0 && mesh->primitives[0].type == cgltf_primitive_type_triangles);
+		cgltf_primitive* firstPrimitive = &mesh->primitives[0];
+
+		/* expect exactly two attributes in the primitive - position and texture coordinates */
+		BreakIfFalse(firstPrimitive->attributes_count == 2);
+		BreakIfFalse(firstPrimitive->attributes[0].type == cgltf_attribute_type_position);
+		BreakIfFalse(firstPrimitive->attributes[1].type == cgltf_attribute_type_texcoord);
+		cgltf_attribute* position = &firstPrimitive->attributes[0];
+		cgltf_attribute* textureCoordinates = &firstPrimitive->attributes[1];
+
+		/* expect the count of positions matches the count of texture coordinates */
+		cgltf_size const vertexCount{ position->data->count };
+		BreakIfFalse(vertexCount == textureCoordinates->data->count);
+
+		// K, need to load buffers for positions and tex coordinates
+		// but I don't have an URI ? it is null
+		//textureCoordinates->data->buffer_view->buffer[]
+
+		Mesh outMesh{};
+		for (int i = 0; i < vertexCount; ++i)
+		{
+			//outMesh.vertices.emplace_back(
+			//	static_cast<float>(assimpVertex.x),
+			//	static_cast<float>(assimpVertex.y),
+			//	static_cast<float>(assimpVertex.z),
+			//	1.0f, // w
+			//	UV.x, UV.y
+			//);
+		}
+		outModel.meshes.emplace_back(std::move(outMesh));
+
+		cgltf_free(data);
+		return true;
+	}
 }
 
 namespace gg
@@ -77,7 +140,8 @@ namespace gg
 	void LoadMeshes(std::string const& modelRelativePath, Model& model)
 	{
 		std::string modelFileAbsPath{ std::filesystem::absolute(modelRelativePath).generic_string() };
-		if (!LoadMeshesAssimp(modelFileAbsPath, model))
+		//if (!LoadMeshesAssimp(modelFileAbsPath, model))
+		if (!LoadMeshesCGLTF(modelFileAbsPath, model))
 			throw AssetLoadException(std::format("Failed to read the input model: {}", modelFileAbsPath));
 	}
 
